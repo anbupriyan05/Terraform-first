b1bc26a46078ed656c7d5dd1dae82932cf8dbab2
anbupriyan05 <anbupriyanms05@gmail.com>
2026-03-05 00:19:16 +0530

    added main and provider file

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "security-group" {
 module "jenkins" {
   source                    = "./jenkins"
   ami_id                    = var.ec2_ami_id
-  instance_type             = "t2.micro"
+  instance_type             = "t3.micro"
   tag_name                  = "Jenkins:Ubuntu Linux EC2"
   public_key                = var.public_key
   subnet_id                 = tolist(module.networking.dev_proj_1_public_subnets)[0]
