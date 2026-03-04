8785ff2dcd19516207bc977e653df5b43c920993
anbupriyan05 <anbupriyanms05@gmail.com>
2026-03-04 19:42:54 +0530

    added main and provider file

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "jenkins_ec2_instance_ip" {
   tags = {
     Name = var.tag_name
   }
-  key_name                    = "practice-terraform"
+  key_name                    = "practice-1"
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.sg_for_jenkins
   associate_public_ip_address = var.enable_public_ip_address
@@ -39,6 +39,6 @@ resource "aws_instance" "jenkins_ec2_instance_ip" {
 }
 
 resource "aws_key_pair" "jenkins_ec2_instance_public_key" {
-  key_name   = "practice-terra"
+  key_name   = "practice-1"
   public_key = var.public_key
 }
